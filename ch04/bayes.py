cd4091ac997830174ace5b36e41929e46c9f67ad
WANG_Xuesong <wangxuesong29@gmail.com>
2017-01-05 20:14:48 +0100

    增加注释，并测试函数

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -8,12 +10,13 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1]    # 1代表侮辱性文字
     return postingList, classVec
 
-# 创建一个包含在所有文档中出现的不重复的列表
+#使用set类型，可以创建一个包含在所有文档中出现的不重复的词汇列表
 def createVocabList(dataSet):
     vocabSet = set([])          # 创建一个空集
     for doucment in dataSet:
         vocabSet = vocabSet | set(doucment)#创建两个集合的并集
     return list(vocabSet)
+
 
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)          #创建一个所有元素都为0的向量
@@ -24,3 +27,11 @@ def setOfWords2Vec(vocabList, inputSet):
     return returnVec
 
 lis, cla = loadDataSet()
+print lis
+print cla
+
+dataSetNoCopy = createVocabList(lis)
+print dataSetNoCopy
+
+print setOfWords2Vec(dataSetNoCopy, lis[0])
+print setOfWords2Vec(dataSetNoCopy, lis[3])
