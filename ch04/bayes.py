a5410c8f2b29b68e7f7e77f312774920e2e3d6d7
WANG_Xuesong <wangxuesong29@gmail.com>
2017-01-05 21:46:33 +0100

    计算一个词被判定为正常和不和谐的概率

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import numpy as np
+
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -8,6 +10,7 @@ def loadDataSet():
                  ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0,1,0,1,0,1]    # 1代表侮辱性文字
+    print len(postingList)
     return postingList, classVec
 
 #使用set类型，可以创建一个包含在所有文档中出现的不重复的词汇列表
@@ -26,12 +29,50 @@ def setOfWords2Vec(vocabList, inputSet):
         else: print ("the word %s is not in my vocabulary!") % word
     return returnVec
 
+
+def trainNBO(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    print("numWords是")
+    print numWords
+    pAbusive = sum(trainCategory)/float(numTrainDocs)#文档属于该屏蔽范畴的概率
+    p0Num = np.zeros(numWords)
+    p1Num = np.zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])# trainMatrix[i] 是无重复词汇数组在第一个词汇本下的零一分布。
+    p1Vect = p1Num/p1Denom
+    p0Vect = p0Num/p0Denom
+    return p0Vect, p1Vect, pAbusive
+
+
 lis, cla = loadDataSet()
-print lis
-print cla
+#print lis
+#print cla
 
 dataSetNoCopy = createVocabList(lis)
-print dataSetNoCopy
+#print dataSetNoCopy
 
-print setOfWords2Vec(dataSetNoCopy, lis[0])
-print setOfWords2Vec(dataSetNoCopy, lis[3])
+#print setOfWords2Vec(dataSetNoCopy, lis[0])
+#print setOfWords2Vec(dataSetNoCopy, lis[3])
+
+trainMat = []
+for postinDoc in lis:
+    print postinDoc
+    trainMat.append(setOfWords2Vec(dataSetNoCopy, postinDoc))
+    print np.size(trainMat)
+
+
+print len(trainMat)
+print trainMat[2]
+print sum(trainMat[2])
+
+print trainNBO(trainMat, cla)[0]
+#print trainNBO(trainMat, cla)[1]
+#print trainNBO(trainMat, cla)[2]
