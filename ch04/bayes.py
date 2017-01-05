b9653d7e90dca7eb14191a8829f987317ee6d7e0
WANG_Xuesong <wangxuesong29@gmail.com>
2017-01-05 22:38:37 +0100

    small test

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -20,7 +20,7 @@ def createVocabList(dataSet):
         vocabSet = vocabSet | set(doucment)#创建两个集合的并集
     return list(vocabSet)
 
-
+#把无重复数据按照原文第一二三。。
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)          #创建一个所有元素都为0的向量
     for word in inputSet:
@@ -33,8 +33,6 @@ def setOfWords2Vec(vocabList, inputSet):
 def trainNBO(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
-    print("numWords是")
-    print numWords
     pAbusive = sum(trainCategory)/float(numTrainDocs)#文档属于该屏蔽范畴的概率
     p0Num = np.ones(numWords)
     p1Num = np.ones(numWords)
@@ -51,6 +49,28 @@ def trainNBO(trainMatrix, trainCategory):
     p0Vect = np.log(p0Num/p0Denom)
     return p0Vect, p1Vect, pAbusive
 
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts, listClasss = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNBO(np.array(trainMat), np.array(listClasss))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, "classified as:", classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ["stupid", "garbage"]
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, "classified as:", classifyNB(thisDoc, p0V, p1V, pAb)
+
 
 lis, cla = loadDataSet()
 #print lis
@@ -64,15 +84,12 @@ dataSetNoCopy = createVocabList(lis)
 
 trainMat = []
 for postinDoc in lis:
-    print postinDoc
     trainMat.append(setOfWords2Vec(dataSetNoCopy, postinDoc))
-    print np.size(trainMat)
 
 
-print len(trainMat)
 print trainMat[2]
 print sum(trainMat[2])
 
 print trainNBO(trainMat, cla)[0]
-#print trainNBO(trainMat, cla)[1]
-#print trainNBO(trainMat, cla)[2]
+
+testingNB()
